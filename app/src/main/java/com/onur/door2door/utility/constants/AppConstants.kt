edb193959fae22ebd3186a7c2f03f342691422e4
Faruk Albayrak <faruk.albayrak@loodos.com>
2021-05-06 14:17:22 +0300

    added socket struc

--- app/src/main/java/com/onur/door2door/utility/constants/AppConstants.kt
+++ app/src/main/java/com/onur/door2door/utility/constants/AppConstants.kt
@@ -5,6 +5,11 @@ package com.onur.door2door.utility.constants
  */
 
 object AppConstants {
-    const val PICKUP_REQUEST_CODE=1001
-    const val DROP_REQUEST_CODE=1002
+    const val PICKUP_REQUEST_CODE = 1001
+    const val DROP_REQUEST_CODE = 1002
+    const val NORMAL_CLOSURE_STATUS = 1000
+    const val RESPONSE_TYPE = "type"
+    const val RESPONSE_STATUS = "status"
+    const val RESPONSE_ERROR_TEXT = "errorText"
+    const val RESPONSE_DATA = "data"
 }
